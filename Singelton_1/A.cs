945a7ef10bfe3c022ee904aeb0d701594a83aece
merts <merts@MERT>
2022-11-28 15:02:01 +0300

    Add project files.

---
+++ Singelton_1/A.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton_1
+{
+    public class A
+    {
+        private static A a;
+        private static readonly object obj = new object();
+        private A()
+        {
+
+        }
+
+        public static A getInstance()
+        {
+            if (a == null)
+            {
+                lock (obj)
+                {
+                    if (a == null)
+                    {
+                        a = new A();
+                    }
+
+                }
+            }
+            return a;
+        }
+    }
+}
